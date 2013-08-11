5444bf01eb866941d3f1d06b143bd5f9a02f224d
Juleffel <juleffel@hotmail.fr>
2013-08-11 16:54:28 +0200

    First commit with a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TimeMapStoryLine::Application.config.secret_key_base = 'caf66476af65bb7c7394a3d0766ce3cbafd6a6aede0c6346ebd2a176b366ab4a8b3991077c23064f5cfff89b1ff7ef298f6f852dcd78d15b00f99995127a35c9'
