ecafc219ebe3da06cb75aef3d2261c485c70efa9
Juleffel <juleffel@hotmail.fr>
2014-03-01 21:56:05 +0100

    cerebro isn't in links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,20 +4,6 @@ class LinksController < ApplicationController
   # GET /links
   # GET /links.json
   def index
-    @characters = Character.includes(:to_links)
-    @characters_by_id = Character.hash_by(:id)
-    @links = Link.all
-    @links_by_id = Link.hash_by(:id)
-    @groups = Group.all
-    @groups_by_id = Group.hash_by(:id)
-    @type_links_by_id = {
-      1 => {
-        "id" => 1,
-        "name" => "All",
-        "link_ids" => Link.all.map(&:id),
-        "color" => "#AAA",
-      }
-    }
   end
 
   # GET /links/1
