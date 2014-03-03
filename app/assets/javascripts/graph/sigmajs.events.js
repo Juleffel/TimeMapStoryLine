9fb51a47ef74727eb7dc16ceaf219d497c1e87f4
pidupuis <pmj.dupuis@gmail.com>
2014-03-03 20:40:47 +0100

    Improve line drawing during scroll and also handle the grph moving

--- app/assets/javascripts/graph/sigmajs.events.js
+++ app/assets/javascripts/graph/sigmajs.events.js
@@ -33,98 +33,127 @@ sigma.publicPrototype.bindEvents = function ($data_container) {
   	popUp && popUp.remove();
   }
   mouseDownOvernodes = function(e) {
-  	// When clic on node without release : start line drawing
-  	popUp && popUp.remove();
-  	mouseDown = true;
-  	
-  	sigInst.iterNodes(function (n) {
-		if (n.id == selectedNodeName) {
-			selectedNode = n;
-		}
-	});
-	
-	letsdraw = true;
-    ctx.strokeStyle = 'white';
-    ctx.lineWidth = 1;
-    ctx.lineCap = 'round';
-    debX = selectedNode.displayX;//e.pageX;
-    debY = selectedNode.displayY;//e.pageY;
+  	if( e.which == 1 ) {
+  	   	  selectedNodeName = overNodeName;
+  	   	  
+	  	// When clic on node without release : start line drawing
+	  	popUp && popUp.remove();
+	  	mouseDown = true;
+	  	
+	  	sigInst.iterNodes(function (n) {
+			if (n.id == selectedNodeName) {
+				selectedNode = n;
+			}
+		});
+		
+		letsdraw = true;
+	    ctx.strokeStyle = 'white';
+	    ctx.lineWidth = 1;
+	    ctx.lineCap = 'round';
+	    debX = selectedNode.displayX;//e.pageX;
+	    debY = selectedNode.displayY;//e.pageY;
+    
+    
+   }
   };
   mouseUpOvernodes = function(e) {
-  	// When release clic on node :
-  	//		- if the node is the same as mousedown, we display it info
-  	//		- if the node is a second one, we create a link between the two nodes
   	
-  	if (selectedNode2Name == null || selectedNode2Name == selectedNodeName) {
-  		sigInst.hideNodeinfo();
-    	sigInst.showNodeinfo(selectedNodeName);
+  	if( e.which == 1 ) {
+  	   	  if (overNodeName != selectedNodeName) {
+  	   	  	selectedNode2Name = overNodeName;
+  	   	  }
+
+	  	// When release clic on node :
+	  	//		- if the node is the same as mousedown, we display it info
+	  	//		- if the node is a second one, we create a link between the two nodes
+	  	
+	  	if (selectedNode2Name == null || selectedNode2Name == selectedNodeName) {
+	  		sigInst.hideNodeinfo();
+	    	sigInst.showNodeinfo(selectedNodeName);
+	  	}
+	  	else {
+	  		console.log("Link creation between "+selectedNodeName+ " and "+selectedNode2Name);
+	  		
+	  		sigInst.iterNodes(function (n) {
+			    if (n.id == selectedNodeName) {
+			      selectedNode = n;
+			    }
+			    else if (n.id == selectedNode2Name) {
+			      selectedNode2 = n;
+			    }
+			  });
+	  		
+	  		popUp = $(
+	          '<div class="node-info-popup"></div>'
+	        ).append(
+	      		"Vous souhaitez créer un lien entre "+selectedNode.label+ " et "+selectedNode2.label+" ?"+
+	      		'<br />Titre du lien : <input id="link_title" type="text" placeholder="Entrez un titre">'+
+	      		'<br />Description : <input id="link_descript" type="text" placeholder="Entrez une description">'+
+	      		'<br /><button id="submitNewInt">Confirmer</button>'+'<button id="cancelNewInt">Annuler</button>'
+	    	).attr(
+	          'id',
+	          'link-creation'+sigInst.getID()
+	        ).css({
+	          'display': 'inline-block',
+	          'border-radius': 3,
+	          'padding': 5,
+	          'background': '#fff',
+	          'color': '#000',
+	          'box-shadow': '0 0 4px #666',
+	          'position': 'absolute',
+	          'z-index': 10,
+	          'left': '50%',
+	          'top': '50%'
+	        });
+	        
+	        $(document.getElementById('js-graph')).append(popUp);
+	        
+		    $('#submitNewInt').click(submitNewInteraction);
+		    $('#cancelNewInt').click(cancelNewInteraction);
+
+	  	}
+	    
+	    // At the end of mouse release, we return at the beginning state 
+	    mouseDown = false;
+	  	selectedNodeName = null;
+	  	selectedNode2Name = null;
+	  	letsdraw = false;
+	  	ctx.clearRect(0, 0, dom.offsetWidth, dom.offsetHeight);
+
   	}
-  	else {
-  		console.log("Link creation between "+selectedNodeName+ " and "+selectedNode2Name);
-  		
-  		sigInst.iterNodes(function (n) {
-		    if (n.id == selectedNodeName) {
-		      selectedNode = n;
-		    }
-		    else if (n.id == selectedNode2Name) {
-		      selectedNode2 = n;
-		    }
-		  });
-  		
-  		popUp = $(
-          '<div class="node-info-popup"></div>'
-        ).append(
-      		"Vous souhaitez créer un lien entre "+selectedNode.label+ " et "+selectedNode2.label+" ?"+
-      		'<br />Titre du lien : <input id="link_title" type="text" placeholder="Entrez un titre">'+
-      		'<br />Description : <input id="link_descript" type="text" placeholder="Entrez une description">'+
-      		'<br /><button id="submitNewInt">Confirmer</button>'+'<button id="cancelNewInt">Annuler</button>'
-    	).attr(
-          'id',
-          'link-creation'+sigInst.getID()
-        ).css({
-          'display': 'inline-block',
-          'border-radius': 3,
-          'padding': 5,
-          'background': '#fff',
-          'color': '#000',
-          'box-shadow': '0 0 4px #666',
-          'position': 'absolute',
-          'z-index': 10,
-          'left': selectedNode2.displayX,
-          'top': selectedNode2.displayY+15
-        });
-        
-        $(document.getElementById('js-graph')).append(popUp);
-        
-	    $('#submitNewInt').click(submitNewInteraction);
-	    $('#cancelNewInt').click(cancelNewInteraction);
-  		
-        
-        
-  		
-  	}
-    
-    // At the end of mouse release, we return at the beginning state 
-    mouseDown = false;
-  	selectedNodeName = null;
-  	selectedNode2Name = null;
-  	letsdraw = false;
-  	ctx.clearRect(0, 0, dom.offsetWidth, dom.offsetHeight);
   };
   mouseDownOutnodes = function(e) {
-  	popUp && popUp.remove();
+  	if( e.which == 1 ) {
+  		popUp && popUp.remove();
+    }
   };
   mouseUpOutnodes = function(e) {
-    // At the end of mouse release, we return at the beginning state
-  	mouseDown = false;
-  	selectedNodeName = null;
-  	selectedNode2Name = null;
-  	letsdraw = false;
-  	ctx.clearRect(0, 0, dom.offsetWidth, dom.offsetHeight);
+  	if( e.which == 1 ) {
+  	
+	    // At the end of mouse release, we return at the beginning state
+	  	mouseDown = false;
+	  	selectedNodeName = null;
+	  	selectedNode2Name = null;
+	  	letsdraw = false;
+	  	ctx.clearRect(0, 0, dom.offsetWidth, dom.offsetHeight);
+	}
   };
   
   dom.addEventListener('mousemove', function(e) {
     // On mouse move, we draw the line if there is a clicked node
+    
+    if (mouseDown == true) {
+  		sigInst.iterNodes(function (n) {
+			if (n.id == selectedNodeName) {
+				selectedNode = n;
+			}
+		});
+  		
+  		// Voir pour choper un update de selectedNode, en bouclant sur les noeuds pour recup le noeud qui a le meme id
+	    debX = selectedNode.displayX;//e.pageX;
+	    debY = selectedNode.displayY;//e.pageY;
+  	}
+  	  
     if (letsdraw === true) {
        ctx.clearRect(0, 0, dom.offsetWidth, dom.offsetHeight);
        ctx.beginPath();
@@ -132,6 +161,8 @@ sigma.publicPrototype.bindEvents = function ($data_container) {
        ctx.lineTo(e.pageX, e.pageY);
        ctx.stroke();
     }
+    
+    
   }, false);
  
   sigInst.bind('overnodes', function (event) {
@@ -142,27 +173,22 @@ sigma.publicPrototype.bindEvents = function ($data_container) {
     
     var nodes = event.content;
     sigInst.selectNodes(nodes);
-    if (!mouseDown) {
-    	selectedNodeName = event.content[0];
-    }
-    else {
-    	selectedNode2Name = event.content[0];
-    }
+    overNodeName = event.content[0];
     sigInst.mouseProperties({
 		mouseEnabled: false
 	});
 	
 	// Remove event listeners for outnodes
-	dom.removeEventListener('mousedown', mouseDownOutnodes);
-	dom.removeEventListener('mouseup', mouseUpOutnodes);
+	$(window).off('mousedown', mouseDownOutnodes);
+	$(window).off('mouseup', mouseUpOutnodes);
 	
 	// Add mouse down listener
-	dom.removeEventListener('mousedown', mouseDownOvernodes);
-	dom.addEventListener('mousedown', mouseDownOvernodes);
+	$(window).off('mousedown', mouseDownOvernodes);
+	$(window).on('mousedown', mouseDownOvernodes);
 	
 	// Add mouse up listener
-	dom.removeEventListener('mouseup', mouseUpOvernodes);
-	dom.addEventListener('mouseup', mouseUpOvernodes);
+	$(window).off('mouseup', mouseUpOvernodes);
+	$(window).on('mouseup', mouseUpOvernodes);
 	
   }).bind('outnodes', function (event) {
     // When we put the mouse cursor outside nodes :
@@ -171,24 +197,24 @@ sigma.publicPrototype.bindEvents = function ($data_container) {
     //		- we modify some custom mouse listener
     
     sigInst.deselectNodes();
-    if (!mouseDown) {
-    	selectedNodeName = null;
-    }
+    
+    overNodeName = null;
+	
+	// Remove event listeners for overnodes
+	$(window).off('mousedown', mouseDownOvernodes);
+	$(window).off('mouseup', mouseUpOvernodes);
+	
+	// Add mouse down listener
+	$(window).off('mousedown', mouseDownOutnodes);
+	$(window).on('mousedown', mouseDownOutnodes);
+	
+	// Add mouse up listener
+	$(window).off('mouseup', mouseUpOutnodes);
+	$(window).on('mouseup', mouseUpOutnodes);
+	
     sigInst.mouseProperties({
 		mouseEnabled: true
 	});
-	
-	// Remove event listeners for overnodes
-	dom.removeEventListener('mousedown', mouseDownOvernodes);
-	dom.removeEventListener('mouseup', mouseUpOvernodes);
-	
-	// Add mouse down listener
-	dom.removeEventListener('mousedown', mouseDownOutnodes);
-	dom.addEventListener('mousedown', mouseDownOutnodes);
-	
-	// Add mouse up listener
-	dom.removeEventListener('mouseup', mouseUpOutnodes);
-	dom.addEventListener('mouseup', mouseUpOutnodes);
 	
   }).bind('downgraph', function (event) {
     // When we click on the graph : we hide node info and do nothing else
@@ -212,28 +238,7 @@ sigma.publicPrototype.bindEvents = function ($data_container) {
     $.data(this, 'scrollTimer', setTimeout(function () {
       /* Function called after 250ms without scrolling. */
       sigInst.updateTypeNodeFilter();
-    
-    
     }, 250));
-    clearTimeout($.data(this, 'scrollTimer'));
-    $.data(this, 'scrollTimer', setTimeout(function () {
-      /* Function called after 100ms without scrolling. */  
-      if (mouseDown == true) {
-  		sigInst.iterNodes(function (n) {
-			if (n.id == selectedNodeName) {
-				selectedNode = n;
-			}
-		});
-  		
-  		console.log(selectedNode.displayX+" "+selectedNode.displayY);
-  		// Voir pour choper un update de selectedNode, en bouclant sur les noeuds pour recup le noeud qui a le meme id
-	    debX = selectedNode.displayX;//e.pageX;
-	    debY = selectedNode.displayY;//e.pageY;
-  	  }
-    }, 250));
-    
-    
-    
   };
   /*** END : REFRESH NODES ***/
  
